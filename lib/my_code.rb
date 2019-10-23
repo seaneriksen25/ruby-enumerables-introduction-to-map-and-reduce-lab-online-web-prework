abc909eef9690d5cc7be197d391c0625520deb2b
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 19:56:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # My Code here....
+map_to_negativize([1, 2, 3], x-1) => []
+
+map_to_no_change([1, 2, 3] +0)
+
+map_to_double([1, 2, 3] x2) => []
+
+map_to_square([1, 2, 3] **1) => []
+
+
+reduce_to_total([1, 2, 3] +0) 
+
+reduce_to_all_true
+
+reduce_to_any_true
+
