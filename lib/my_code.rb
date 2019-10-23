9dc05cb07b1d48edceeaed5bba880b2c08a523be
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 20:01:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,12 +3,12 @@ map_to_negativize([1, 2, 3], x-1) => []
 
 map_to_no_change([1, 2, 3] +0)
 
-map_to_double([1, 2, 3] x2) => []
+map_to_double([1, 2, 3] x 2) => []
 
 map_to_square([1, 2, 3] **1) => []
 
 
-reduce_to_total([1, 2, 3] +0) 
+reduce_to_total(2..5).inject { |sum, n| sum + n } 
 
 reduce_to_all_true
 
