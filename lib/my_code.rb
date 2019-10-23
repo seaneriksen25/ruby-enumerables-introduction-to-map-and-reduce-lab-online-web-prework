07670a8b5c0d6961b229d3670449f2ae01738bf3
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 20:04:08 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # My Code here....
+map_to_negativize([1, 2, 3], x-1) => []
+
+map_to_no_change([1, 2, 3] +0)
+
+map_to_double([1, 2, 3] x 2) => []
+
+map_to_square([1, 2, 3] **1) => []
+
+
+reduce_to_total(2..5).inject { |sum, n| sum + n } 
+
+reduce_to_all_true
+
+reduce_to_any_true
+
